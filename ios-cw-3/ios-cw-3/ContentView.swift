48f0a15b01e343087cad532c4a4f3e61bca84113
shaikaalobaid <salobaid250@gmail.com>
2022-03-05 12:04:49 +0300

    added my fav character

--- ios-cw-3/ios-cw-3/ContentView.swift
+++ ios-cw-3/ios-cw-3/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+        Text("my favourite characters")
+            .font(.largeTitle)
+            .fontWeight(.thin)
+            .foregroundColor(Color.purple)
             .padding()
+            HStack{
+                VStack{
+            Image("rick and morty")
+                .resizable()
+                .frame(width:100, height: 100)
+              
+                    Text("rick and morty")
+                }
+                VStack{
+            Image("heisenberg")
+                .resizable()
+                .frame(width:100, height: 100)
+                    Text("heisenberg")}}
+        
+            HStack{
+                VStack{
+            Image("amy santiago")
+                .resizable()
+                .frame(width:100, height: 100)
+                    Text("amy santiago")}
+                VStack{
+            Image("michael scofield")
+                .resizable()
+                .frame(width:100, height: 100)
+                    Text("micheal scofield")}
+
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
+  }
 }
